33a4e9d1dc6c457c3812fdb0bbe87ee871fd073c
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-04-03 12:36:37 +0200

    Added greeting and ASCII logo

--- CybersecurityChatbot/Program.cs
+++ CybersecurityChatbot/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Media;
+using System.Threading;
+
+class CyberSecurityChatBot
+{
+  static void Main()
+  {
+    //voice greeting
+    PlayVoiceGreeting();
+
+    //ASCII logo
+    DisplayAsciiLogo();
+
+    //getting and using users name for personal interactoion
+    Console.Write("Please enter your name: ");
+    string userName = Console.ReadLine();
+    Console.WriteLine($"\nHello, {userName}! Welcome to the Cybersecurity Awareness Bot.\n");
+  }
+
+  static void PlayVoiceGreeting()
+  {
+    try
+    {
+      SoundPlayer player = new SoundPlayer("greeting.wav");
+      player.Play();
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine("(Voice Greeting Error:" + ex.Message + ")");
+    }
+  }
+
+  static void DisplayAsciiLogo()
+  {
+    Console.WriteLine(@"
+     _   _            _        _           _ _       _
+    | | | |          (_)      | |         | (_)     | |
+    | |_| |__   __ _ _ _ __   _| |_ ___  __ _ _ _ __ _| |_ ___
+    | __| '_ \ / _` | | '_ \ | | __/ _ \/ _` | | '__| | __/ _ \
+    | |_| | | | (_| | | | | | | ||  __/ (_| | | |  | | ||  __/
+    \__|_| |_|\__,_|_|_| |_|_|\__\___|\__,_|_|_|  |_|___\___|");
+    Console.WriteLine("\n=================================================");
+  }
+}
